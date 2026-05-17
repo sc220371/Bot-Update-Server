b24b57da2bcf4d294f79d38776e1972ff9ebf8ac
sc220371 <sc220371@gmail.com>
2026-05-17 10:00:55 +0800

    Update version.js

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
 {
   "latest_version": 10.7,
-  "download_url": "https://https://drive.google.com/file/d/1vHZkP7Cy102sgDDCC1B5SPbyolvhjYqZ/view?usp=drive_link.com/update.zip"
+  "download_url": "https://github.com/sc220371/Bot-Update-Server/releases/download/v10.7/_10.7.zip"
 }
